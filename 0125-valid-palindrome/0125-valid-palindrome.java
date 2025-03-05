131a51b24dc8a7109f7ad7fa91b4732aae623eca
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-03-05 12:20:52 +0530

    Time: 4 ms (58.38%), Space: 44 MB (50.07%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+        String filteredStr = filtered.toString();
+        String reversedStr = filtered.reverse().toString();
+        return filteredStr.equals(reversedStr);
+    }
+}
