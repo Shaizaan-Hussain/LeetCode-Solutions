f539d7a2e287f75a5ed97c80361984123b51901f
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-07-15 10:36:35 +0530

    Time: 1 ms (99.12%), Space: 41.9 MB (90.73%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,23 +1,29 @@
 class Solution {
+
     public boolean isValid(String word) {
-        if (word.length() < 3) return false;
-
-        if (!word.matches("[a-zA-Z0-9]+")) return false;
-
+        if (word.length() < 3) {
+            return false;
+        }
         boolean hasVowel = false;
         boolean hasConsonant = false;
-
         for (char c : word.toCharArray()) {
             if (Character.isLetter(c)) {
                 char ch = Character.toLowerCase(c);
-                if ("aeiou".indexOf(ch) != -1) {
+                if (
+                    ch == 'a' ||
+                    ch == 'e' ||
+                    ch == 'i' ||
+                    ch == 'o' ||
+                    ch == 'u'
+                ) {
                     hasVowel = true;
                 } else {
                     hasConsonant = true;
                 }
+            } else if (!Character.isDigit(c)) {
+                return false;
             }
         }
         return hasVowel && hasConsonant;
-
-        }
     }
+}
