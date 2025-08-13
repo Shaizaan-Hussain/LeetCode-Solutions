96d004d3b3e0b5275f3bfc327ee2779b055de22c
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-08-13 12:42:26 +0530

    Time: 1 ms (62.02%), Space: 46.2 MB (73.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,51 +1,29 @@
 class Solution {
     public int trap(int[] height) {
-
-        // int n = height.length;
-        // int prefix[] = new int[n];
-        // int suffix[] = new int[n];
-        // prefix[0] = height[0];
-        // for (int i = 1; i < n; i++) {
-        //     prefix[i] = Math.max(prefix[i - 1], height[i]);
-        // }
-        // suffix[n - 1] = height[n - 1];
-        // for (int i = n - 2; i >= 0; i--) {
-        //     suffix[i] = Math.max(suffix[i + 1], height[i]);
-        // }
-        // int waterTrapped = 0;
-        // for (int i = 0; i < n; i++) {
-        //     waterTrapped += Math.min(prefix[i], suffix[i]) - height[i];
-        // }
-        // return waterTrapped;
-
         int n = height.length;
-        int left = 0;
-        int right = n-1;
-        int lmax=height[0];
-        int rmax=height[n-1];
 
-        int result =0;
-
-        while(left<=right){
-
-            if(height[left]<=height[right]){
-                if(height[left]>=lmax){
-                    lmax=height[left];
-                }else{
-                    result += (lmax-height[left]);
-                }
-                left++;
-            }else{
-                if(height[right]>=rmax){
-                    rmax=height[right];
-                }else{
-                    result += (rmax-height[right]);
-                }
-                right--;
-            }
+        // Left maximum bar auxillary array
+        int[] left = new int[n];
+        left[0] = height[0];
+        for(int i = 1; i< n;i++){
+            left[i] = Math.max(height[i], left[i-1]);
         }
 
-        return result;
-        
+
+         //Right maximum bar auxillary array
+         int[] right = new int[n];
+         right[n-1] = height[n-1];
+         for(int i = n-2; i>=0;i--){
+            right[i] = Math.max(height[i], right[i+1]);
+         }
+
+         //claculating trapped water
+         int trappedWater = 0;
+         for(int i =0;i<n;i++){
+            int waterLevel = Math.min(left[i], right[i]);
+            trappedWater += waterLevel - height[i];
+         }
+
+        return trappedWater;
     }
 }
