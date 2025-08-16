26d97c50ca80a5cb1d29fe34d876b8a26572f2af
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-08-16 10:13:47 +0530

    Time: 0 ms (100%), Space: 40.7 MB (44.6%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,18 +1,12 @@
 class Solution {
     public int maximum69Number(int num) {
-        String str = String.valueOf(num);
-        int max = num;  // start with original number
-
-        for (int i = 0; i < str.length(); i++) {
-            char[] chars = str.toCharArray(); // make a copy
-            if (chars[i] == '9') {
-                chars[i] = '6';
-            } else {
-                chars[i] = '9';
+  char[] digits = String.valueOf(num).toCharArray();  
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; 
             }
-            int curr = Integer.valueOf(new String(chars));
-            max = Math.max(max, curr);
         }
-        return max;
+        return Integer.parseInt(new String(digits));
     }
 }
